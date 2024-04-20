5d8e259e67e97926192fec81c2b3dcced6c8335a
Henry4593 <jonyango4@gmail.com>
2024-04-20 20:11:26 +0300

    fixed bugs

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,5 @@
-#!/usr/bin/env bash
 # make changes to 'ssh_config' file using puppet codes
-file{'/etc/ssh/ssh_config':
+file{ '/etc/ssh/ssh_config':
     ensure => present,	
     content =>"
     	#SSH client configuration
