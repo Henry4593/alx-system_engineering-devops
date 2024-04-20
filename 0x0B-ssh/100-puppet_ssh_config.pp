20f74fa2d45fc3bccad353bfe33501b43984d7cc
Henry4593 <jonyango4@gmail.com>
2024-04-20 20:13:57 +0300

    fixed bugs

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,20 @@
 # make changes to 'ssh_config' file using puppet codes
-file{ '/etc/ssh/ssh_config':
-    ensure => present,	
-    content =>"
-    	#SSH client configuration
-	host*
-	IdentityFile ~/.ssh/school
-	PasswordAunthentication no
-	",
+file { '/etc/ssh/ssh_config':
+  # Ensure the file is present
+  ensure => present,
+  # Define the content of the file
+  content => <<EOF
+# SSH client configuration
+Host *
+  # Use the SSH key from ~/.ssh/school
+  IdentityFile  ~/.ssh/school
+  # Disable password authentication
+  PasswordAuthentication no
+EOF
+  # Owner and group permissions (optional)
+  # owner => 'root',
+  # group => 'root',
+  # File permissions (optional)
+  # mode => '0644',
 }
+
