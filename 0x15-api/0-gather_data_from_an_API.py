2227d360229a1dc43d5d1b178ab440a66a509128
Henry4593 <jonyango4@gmail.com>
2024-05-24 00:16:14 +0300

    fixed bugs

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,26 +6,18 @@ import sys
 
 
 if __name__ == "__main__":
-    # Check if the script was called with an argument
     if len(sys.argv) != 2:
         print("Usage: python3 script.py <employee_id>")
         sys.exit(1)
 
-    # Get the employee ID from the command-line argument
     employee_id = int(sys.argv[1])
-    # API endpoint
     users_url = "https://jsonplaceholder.typicode.com/users/"
-
-    # API endpoint
     todos_url = "https://jsonplaceholder.typicode.com/todos/"
 
-    # Make the request
     todos_res = requests.get(todos_url)
     users_res = requests.get(users_url)
 
-    # Check if the request was successful
     if todos_res.status_code == 200 and users_res.status_code == 200:
-        # Parse the JSON data
         todos = todos_res.json()
         users = users_res.json()
         employee_name = users[employee_id - 1]["name"]
