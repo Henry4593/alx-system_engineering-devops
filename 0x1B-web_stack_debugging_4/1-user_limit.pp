e8fe423c2dbd2f5f39dcd06c908943e57e461745
Henry4593 <jonyango4@gmail.com>
2024-06-10 15:14:21 +0300

    initial commit

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,7 @@
+# Set up the OS to allow login as the holberton user and open files without
+# encountering error messages.
+
+exec {'OS security config':
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
+}
---
+++ 0x1B-web_stack_debugging_4/README.md
@@ -0,0 +1,1 @@
+This is the documentation for project 0x1B-web_stack_debugging_4
