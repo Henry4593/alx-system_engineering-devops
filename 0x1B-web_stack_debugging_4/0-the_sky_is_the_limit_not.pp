87e619df4d8bc96f6c2aa7ab3e9de1eba597ae8b
Henry4593 <jonyango4@gmail.com>
2024-08-19 13:48:51 +0300

    initial commit

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# increases traffic threshold for nginx
+
+# Increase the ULIMIT of the nginx
+exec { 'increase user limit':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+# restarts nginx
+exec { 'restart nginx':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
